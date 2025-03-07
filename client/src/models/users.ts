04ce81b634cf3298fcfcdb8ab1dbc233f97b2121
Fv40 <deckerd70@yahoo.com>
2025-03-07 11:48:24 -0500

    Formatting

--- client/src/models/users.ts
+++ client/src/models/users.ts
@@ -1,40 +1,40 @@
 import { ref } from 'vue'
 
 interface User {
-    id: number;
-    name: string;
-    isAdmin: boolean;
+  id: number
+  name: string
+  isAdmin: boolean
 }
 
 const users = ref<User[]>([
-    { 
-        id: 1,
-        name: 'John Doe',
-        isAdmin: false
-    },
-    { 
-        id: 2, 
-        name: 'Jane Doe', 
-        isAdmin: false
-    },
-    { 
-        id: 3, 
-        name: 'John Smith', 
-        isAdmin: false
-    },
-    { 
-        id: 4, 
-        name: 'Spongebob',
-        isAdmin: true
-    }
+  {
+    id: 1,
+    name: 'John Doe',
+    isAdmin: false,
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    isAdmin: false,
+  },
+  {
+    id: 3,
+    name: 'John Smith',
+    isAdmin: false,
+  },
+  {
+    id: 4,
+    name: 'Spongebob',
+    isAdmin: true,
+  },
 ])
 
 export const currentUser = ref<User | null>(null)
 
 export function getUsers() {
-    return users
+  return users
 }
 
 export function selectUser(user: User) {
-    currentUser.value = user
+  currentUser.value = user
 }
