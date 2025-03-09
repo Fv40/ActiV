7443a4c9e6f80f7d25bbd70b1b5ae8767ffd5634
Fv40 <deckerd70@yahoo.com>
2025-03-08 20:38:51 -0500

    WEB-10: fixed info box text expanding when page is stretch horizontally

--- client/src/components/InfoBox.vue
+++ client/src/components/InfoBox.vue
@@ -34,16 +34,16 @@ const workoutStats = ref({
             </h1>
             <div class="columns is-mobile is-centered">
               <div class="column has-text-centered">
-                <p><u>Distance Walked</u></p>
-                <p>{{ item.distance }} mi</p>
+                <p class ="entry-title"><u>Distance<br>Walked</u></p>
+                <p class="entry">{{ item.distance }} mi</p>
               </div>
               <div class="column has-text-centered">
-                <p><u>Time Worked</u></p>
-                <p>{{ item.duration }} min</p>
+                <p class="entry-title"><u>Time<br>Worked</u></p>
+                <p class="entry">{{ item.duration }} min</p>
               </div>
               <div class="column has-text-centered">
-                <p><u>Calories Consumed</u></p>
-                <p>{{ item.calories }} cal</p>
+                <p class="entry-title"><u>Calories<br>Consumed</u></p>
+                <p class="entry">{{ item.calories }} cal</p>
               </div>
             </div>
           </div>
