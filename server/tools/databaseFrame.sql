6c466c3397918070f1fceb742fe6739b7ad5d18d
Fv40 <deckerd70@yahoo.com>
2025-04-23 19:34:59 -0400

    WEB-35: db change

--- server/tools/databaseFrame.sql
+++ server/tools/databaseFrame.sql
@@ -1,7 +1,7 @@
 -- Creation statements for DB tables
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
     profile_picture_source VARCHAR(255),
     email VARCHAR(100) NOT NULL UNIQUE,
     friendgroups INT[] DEFAULT '{}',
