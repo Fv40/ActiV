181d9cf1fb9591ea3df3c343f0afe4ea48dee570
Fv40 <deckerd70@yahoo.com>
2025-04-24 23:08:04 -0400

    Connected my activity and friends activity to backend logic

--- client/src/models/activity.ts
+++ client/src/models/activity.ts
@@ -1,72 +1,24 @@
-import { ref } from 'vue'
+import * as session from '@/models/connection/session'
 
 export interface Activity {
-  userId: number
-  imageSource: string
-  title: string
-  type: string
-  date: Date
+  activity_id?: number
+  user_id: number
+  thumbnail_src: string
+  activity_description: string
+  activity_type: string
+  activity_date: Date
 }
 
-const activities = ref<Activity[]>([
-  {
-    userId: 4,
-    imageSource: 'https://static2.bigstockphoto.com/5/3/9/large2/93519371.jpg',
-    title: 'Ran down the road',
-    type: 'Cardio',
-    date: new Date(2025, 2, 9),
-  },
-  {
-    userId: 2,
-    imageSource:
-      'https://web-back.perfectgym.com/sites/default/files/styles/900x/public/equipment%20(6).jpg?itok=UbaJqjIr',
-    title: 'Lifted dumbells',
-    type: 'Strength',
-    date: new Date(2025, 2, 7),
-  },
-  {
-    userId: 3,
-    imageSource:
-      'https://halfmarathonforbeginners.com/wp-content/uploads/2017/06/AdobeStock_91059214.jpeg',
-    title: 'Ran a marathon',
-    type: 'Cardio',
-    date: new Date(2025, 2, 5),
-  },
-  {
-    userId: 2,
-    imageSource:
-      'https://www.proteinreview.com.au/wp-content/uploads/2018/08/how-to-deadlift-hero.jpg',
-    title: 'Deadlifts',
-    type: 'Strength',
-    date: new Date(2025, 2, 8),
-  },
-  {
-    userId: 1,
-    imageSource:
-      'https://images6.fanpop.com/image/photos/43300000/Spongebob-jellyfishing-spongebob-squarepants-43381957-1200-1203.jpg',
-    title: 'Jellyfishing',
-    type: 'Cardio',
-    date: new Date(2025, 2, 6),
-  },
-  {
-    userId: 1,
-    imageSource: 'https://backend.everyfra.me/spongebob/s1e1/258.png',
-    title: 'Weightlifting',
-    type: 'Strength',
-    date: new Date(2025, 2, 2),
-  },
-])
-
-export function getActivities() {
-  return activities.value.sort((a, b) => b.date.getTime() - a.date.getTime())
+export function getActivities(): Promise<Activity[]> {
+  return session.api("get", "/activities", {})
 }
 
-export function getActivitiesForUser(userId: number) {
-  return activities.value
-    .filter((activity) => activity.userId === userId)
-    .sort((a, b) => b.date.getTime() - a.date.getTime())
+export function getActivitiesForUser(userId: number): Promise<Activity[]> {
+  return session.api("get", `/activities/${userId}`, {})
 }
 
-export function addActivity(activity: Activity) {
-  activities.value.push(activity)
+export function addActivity(activity: Activity):Promise<Activity[]> {
+  return session.api("post", "/activities", {
+    activity
+  })
 }
